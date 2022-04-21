b6aa95f68b73a205a78ed31be7d2f1fb9440065b
German Florez Dussan <German.Florez.Dussan@ey.com>
2022-04-21 14:47:13 -0500

    Creates tests for different scenarion on Math class

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var math = new Fundamentals.Math();
+            var result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Fundamentals.Math();
+            var result = math.Max(2,1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Fundamentals.Math();
+            var result = math.Max(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Fundamentals.Math();
+            var result = math.Max(2, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+    }
+}
