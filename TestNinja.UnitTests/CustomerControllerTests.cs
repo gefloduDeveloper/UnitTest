def49e405dae836e102fd1e36aa76b1317825cc0
German Florez Dussan <German.Florez.Dussan@ey.com>
2022-04-22 12:04:36 -0500

    Different ways of testing methods, it's important to be assertive in the way we test, it is possible to use different ways depending the scenario, for error handling ones, also for void functions

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            //Not Found
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //Not Found or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+
+            //Not Found
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
