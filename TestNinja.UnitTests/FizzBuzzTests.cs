2a09032fbfc92f39b2578e8e86a02a96563b7a7a
German Florez Dussan <German.Florez.Dussan@ey.com>
2022-04-23 11:37:06 -0500

    Solved the Fizzbuzz Test cases for course excersise

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(9,"Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(45, "FizzBuzz")]
+        [TestCase(4, "4")]
+        public void GetOutput_WhenCalled_ReturnTheRightOutput(int a, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(a);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
