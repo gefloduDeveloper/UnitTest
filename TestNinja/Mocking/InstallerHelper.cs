45eb07ae1ad333212da0afc54008ad907f5a96e0
German Florez Dussan <German.Florez.Dussan@ey.com>
2022-04-25 18:14:55 -0500

    Completes second excersise form section 6, refactoring the class and adding the tests

--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -5,18 +5,23 @@ namespace TestNinja.Mocking
     public class InstallerHelper
     {
         private string _setupDestinationFile;
+        private IFileDownloader _fileDownloader;
+
+        public InstallerHelper(IFileDownloader fileDownloader)
+        {
+            _fileDownloader = fileDownloader ?? new FileDownloader();
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
             var client = new WebClient();
             try
             {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
-                    _setupDestinationFile);
-
+                _fileDownloader.DownloadFile(
+                       string.Format("http://example.com/{0}/{1}",
+                           customerName,
+                           installerName),
+                       _setupDestinationFile);
                 return true;
             }
             catch (WebException)
