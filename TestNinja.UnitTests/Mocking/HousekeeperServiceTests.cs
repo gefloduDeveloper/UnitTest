b591375eb58a6f58e4344a4f9e33357d40cc24e7
German Florez Dussan <German.Florez.Dussan@ey.com>
2022-04-28 11:41:01 -0500

    checked the 3 cases when email is not valid

--- TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
+++ TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -50,6 +50,34 @@ namespace TestNinja.UnitTests.Mocking
             sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, _statementDate));
         }
 
-       
+        [Test]
+        public void SendStatementEmails_HouseKeepersEmailIsNull_NotGenerateStatements()
+        {
+            _houseKeeper.Email = null;
+            _service.SendStatementEmails(_statementDate);
+            _statementGenerator.Verify(sg =>
+            sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, _statementDate),
+            Times.Never);
+        }
+
+        [Test]
+        public void SendStatementEmails_HouseKeepersEmailIsWhiteSpace_NotGenerateStatements()
+        {
+            _houseKeeper.Email = " ";
+            _service.SendStatementEmails(_statementDate);
+            _statementGenerator.Verify(sg =>
+            sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, _statementDate),
+            Times.Never);
+        }
+
+        [Test]
+        public void SendStatementEmails_HouseKeepersEmailIsEmpty_NotGenerateStatements()
+        {
+            _houseKeeper.Email = "";
+            _service.SendStatementEmails(_statementDate);
+            _statementGenerator.Verify(sg =>
+            sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, _statementDate),
+            Times.Never);
+        }
     }
 }
